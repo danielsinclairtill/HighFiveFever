cf8f9549de6b1ac71372ced2def74cdf8c61d9bb
Luis <luisabraham_11@hotmail.com>
2016-06-26 00:46:16 -0400

    Created segues to each option in menu. Added background and titles to About and Setting views. Changed status bar color to white to complement purple background.

--- High Five Fever/High Five Fever/MenuViewController.swift
+++ High Five Fever/High Five Fever/MenuViewController.swift
@@ -20,4 +20,7 @@ class MenuViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent;
+    }
 }
